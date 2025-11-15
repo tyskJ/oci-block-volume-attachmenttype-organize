161ad1e71d471dc857899d176e1f56a0470f4298
tyskJ <193552101+tyskJ@users.noreply.github.com>
2025-11-15 15:26:55 +0900

    add vcn resources

--- envs/vcn.tf
+++ envs/vcn.tf
@@ -4,9 +4,38 @@ resource "oci_core_vcn" "vcn" {
   display_name   = "vcn"
 }
 
-resource "oci_core_subnet" "public" {
+resource "oci_core_security_list" "sl" {
   compartment_id = var.compartment_id
   vcn_id         = oci_core_vcn.vcn.id
-  cidr_block     = "10.0.1.0/24"
-  display_name   = "public"
+  display_name   = "nothing-security-list"
+}
+
+resource "oci_core_subnet" "public" {
+  compartment_id    = var.compartment_id
+  vcn_id            = oci_core_vcn.vcn.id
+  cidr_block        = "10.0.1.0/24"
+  display_name      = "public"
+  security_list_ids = [oci_core_security_list.sl.id]
+}
+
+resource "oci_core_internet_gateway" "igw" {
+  compartment_id = var.compartment_id
+  vcn_id         = oci_core_vcn.vcn.id
+  display_name   = "igw"
+}
+
+resource "oci_core_route_table" "rtb" {
+  compartment_id = var.compartment_id
+  vcn_id         = oci_core_vcn.vcn.id
+  display_name   = "rtb"
+  route_rules {
+    network_entity_id = oci_core_internet_gateway.igw.id
+    destination       = "0.0.0.0/0"
+    destination_type  = "CIDR_BLOCK"
+  }
+}
+
+resource "oci_core_route_table_attachment" "attachment" {
+  subnet_id      = oci_core_subnet.public.id
+  route_table_id = oci_core_route_table.rtb.id
 }
