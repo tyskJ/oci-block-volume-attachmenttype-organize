33fc511f9c7ccf7c40ebfa63b3fa2f2d224f9075
tyskJ <193552101+tyskJ@users.noreply.github.com>
2025-11-20 06:17:50 +0000

    add dns label

--- envs/vcn.tf
+++ envs/vcn.tf
@@ -2,6 +2,7 @@ resource "oci_core_vcn" "vcn" {
   compartment_id = var.compartment_id
   cidr_block     = "10.0.0.0/16"
   display_name   = "vcn"
+  dns_label      = "vcn"
 }
 
 resource "oci_core_security_list" "sl" {
@@ -11,11 +12,14 @@ resource "oci_core_security_list" "sl" {
 }
 
 resource "oci_core_subnet" "public" {
-  compartment_id    = var.compartment_id
-  vcn_id            = oci_core_vcn.vcn.id
-  cidr_block        = "10.0.1.0/24"
-  display_name      = "public"
-  security_list_ids = [oci_core_security_list.sl.id]
+  compartment_id             = var.compartment_id
+  vcn_id                     = oci_core_vcn.vcn.id
+  cidr_block                 = "10.0.1.0/24"
+  display_name               = "public"
+  dns_label                  = "public"
+  security_list_ids          = [oci_core_security_list.sl.id]
+  prohibit_internet_ingress  = false
+  prohibit_public_ip_on_vnic = false
 }
 
 resource "oci_core_internet_gateway" "igw" {
