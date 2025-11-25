8a111a2034f8fdb8f5dd41a3642307f1432f06f3
tyskJ <193552101+tyskJ@users.noreply.github.com>
2025-11-25 07:25:14 +0000

    :recycle: 単一AD用にdataリソースを修正

--- envs/data.tf
+++ envs/data.tf
@@ -1,8 +1,9 @@
-data "oci_identity_availability_domains" "ads" {
+data "oci_identity_availability_domain" "ads" {
   compartment_id = var.compartment_id
+  ad_number      = 1
 }
 
 data "oci_identity_fault_domains" "fds" {
   compartment_id      = var.compartment_id
-  availability_domain = data.oci_identity_availability_domains.ads.availability_domains[0].name
+  availability_domain = data.oci_identity_availability_domain.ads.name
 }
