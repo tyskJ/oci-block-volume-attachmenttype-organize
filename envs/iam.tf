615d85badae3583fb8c0234e774fa059def4af22
tyskJ <193552101+tyskJ@users.noreply.github.com>
2025-12-03 22:20:11 +0900

    :sparkles: CloudAgent RunCommand policy 追加

--- envs/iam.tf
+++ envs/iam.tf
@@ -18,13 +18,26 @@ resource "oci_identity_dynamic_group" "compute" {
 /************************************************************
 IAM Policy - Compute
 ************************************************************/
-resource "oci_identity_policy" "compute_for_agent" {
+resource "oci_identity_policy" "compute_for_agent_volmng" {
   compartment_id = oci_identity_compartment.workload.id
-  description    = "OCI Compute Policy for CloudAgent Volume"
-  name           = "compute-cloudagent-volume-policy"
+  description    = "OCI Compute Policy for CloudAgent Volume Management"
+  name           = "compute-cloudagent-volume-management-policy"
   statements = [
     format("allow dynamic-group %s to use instances in compartment %s", oci_identity_dynamic_group.compute.id, oci_identity_compartment.workload.name),
     format("allow dynamic-group %s to use volume-attachments in compartment %s", oci_identity_dynamic_group.compute.id, oci_identity_compartment.workload.name)
+  ]
+  defined_tags = {
+    format("%s.%s", oci_identity_tag_namespace.common.name, oci_identity_tag_default.key_env.tag_definition_name)                = "prd"
+    format("%s.%s", oci_identity_tag_namespace.common.name, oci_identity_tag_default.key_managedbyterraform.tag_definition_name) = "true"
+  }
+}
+
+resource "oci_identity_policy" "compute_for_agent_runcommand" {
+  compartment_id = oci_identity_compartment.workload.id
+  description    = "OCI Compute Policy for CloudAgent RunCommand"
+  name           = "compute-cloudagent-runcommand-policy"
+  statements = [
+    format("allow dynamic-group %s to use instance-agent-command-execution-family in compartment %s", oci_identity_dynamic_group.compute.id, oci_identity_compartment.workload.name)
   ]
   defined_tags = {
     format("%s.%s", oci_identity_tag_namespace.common.name, oci_identity_tag_default.key_env.tag_definition_name)                = "prd"
