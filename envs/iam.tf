aaeffd7cb2aba61f466f4d48cacf183a34db6671
tyskJ <193552101+tyskJ@users.noreply.github.com>
2025-12-03 08:16:45 +0000

    :sparkles: compute用動的グループ作成

--- envs/iam.tf
+++ envs/iam.tf
@@ -1,19 +1,19 @@
 /************************************************************
 Dynamic Group - Compute
 ************************************************************/
-# resource "oci_identity_dynamic_group" "test" {
-#   compartment_id = var.tenancy_id
-#   name           = "test"
-#   description    = "test"
-#   matching_rule = format(
-#     "All {resource.type = 'instance', resource.compartment.id = '%s', tag.Compute.CloudAgent='true'}",
-#     var.compartment_id
-#   )
-#   defined_tags = {
-#     format("%s.%s", oci_identity_tag_namespace.common.name, oci_identity_tag_default.key_env.tag_definition_name)                = "prd"
-#     format("%s.%s", oci_identity_tag_namespace.common.name, oci_identity_tag_default.key_managedbyterraform.tag_definition_name) = "true"
-#   }
-# }
+resource "oci_identity_dynamic_group" "test" {
+  compartment_id = var.tenancy_ocid
+  name           = "Compute_Dynamic_Group"
+  description    = "Compute Dynamic Group"
+  matching_rule = format(
+    "All {resource.type = 'instance', resource.compartment.id = '%s', tag.Compute.CloudAgent.value = 'true'}",
+    oci_identity_compartment.workload.id
+  )
+  defined_tags = {
+    format("%s.%s", oci_identity_tag_namespace.common.name, oci_identity_tag_default.key_env.tag_definition_name)                = "prd"
+    format("%s.%s", oci_identity_tag_namespace.common.name, oci_identity_tag_default.key_managedbyterraform.tag_definition_name) = "true"
+  }
+}
 
 /************************************************************
 IAM Policy - Compute
