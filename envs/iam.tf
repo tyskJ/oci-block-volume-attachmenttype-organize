20c0c5f6f38d175b841a48e7002606d16418700d
tyskJ <193552101+tyskJ@users.noreply.github.com>
2025-12-03 22:54:30 +0900

    :bug: 動的グループの一致ルールを修正

--- envs/iam.tf
+++ envs/iam.tf
@@ -6,7 +6,7 @@ resource "oci_identity_dynamic_group" "compute" {
   name           = "Compute_Dynamic_Group"
   description    = "Compute Dynamic Group"
   matching_rule = format(
-    "All {resource.type = 'instance', resource.compartment.id = '%s', tag.Compute.CloudAgent.value = 'true'}",
+    "All {instance.compartment.id = '%s', tag.Compute.CloudAgent.value = 'true'}",
     oci_identity_compartment.workload.id
   )
   defined_tags = {
