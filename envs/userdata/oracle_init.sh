25c89560135bb70053d118d12a1779283fde2272
tyskJ <193552101+tyskJ@users.noreply.github.com>
2026-01-04 08:29:11 +0900

    :recycle: シェルオプション追加
    Added strict error handling options to the script.

--- envs/userdata/oracle_init.sh
+++ envs/userdata/oracle_init.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# e : エラーがあったら直ちにシェルを終了
+# u : 未定義変数を使用したときにエラーとする
+# o : シェルオプションを有効にする
+# pipefail : パイプラインの返り値を最後のエラー終了値にする (エラー終了値がない場合は0を返す)
+set -euo pipefail
+
 # Timezone
 timedatectl set-timezone Asia/Tokyo
 systemctl restart rsyslog
